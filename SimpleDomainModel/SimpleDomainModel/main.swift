16a1526256a6773a98d708dc09b7190ee44dba8b
karvi90 <karvi90@uw.edu>
2016-04-08 16:50:47 -0700

    Finished Person?

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -11,97 +11,161 @@ import Foundation
 print("Hello, World!")
 
 public func testMe() -> String {
-  return "I have been tested"
+    return "I have been tested"
 }
 
 public class TestMe {
-  public func Please() -> String {
-    return "I have been tested"
-  }
+    public func Please() -> String {
+        return "I have been tested"
+    }
 }
 
 ////////////////////////////////////
 // Money
 //
 public struct Money {
-  public var amount : Int
-  public var currency : String
+    public var amount : Int
+    public var currency : String
   
-  public func convert(to: String) -> Money {
-  }
+    init(amount : Int, currency : String) {
+        self.amount = amount
+        self.currency = currency
+    }
+    
+    public func convert(to: String) -> Money {
+        if (self.currency == "USD" && to == "GBP") {
+            return Money(amount: (self.amount / 2), currency: to)
+        }
+        else if (self.currency == "USD" && to == "EUR") {
+            return Money(amount: ((3 * self.amount) / 2), currency: to)
+        }
+        else if (self.currency == "USD" && to == "CAN") {
+            return Money(amount: (5 * (self.amount) / 4), currency: to)
+        }
+        else if (self.currency == "GBP" && to == "USD") {
+            return Money(amount: (2 * self.amount), currency: to)
+        }
+        else if (self.currency == "GBP" && to == "EUR") {
+            return Money(amount: (3 * self.amount), currency: to)
+        }
+        else if (self.currency == "GBP" && to == "CAN") {
+            return Money(amount: (5 * (self.amount) / 2), currency: to)
+        }
+        else if (self.currency == "EUR" && to == "USD") {
+            return Money(amount: ((2 * self.amount) / 3), currency: to)
+        }
+        else if (self.currency == "EUR" && to == "GBP") {
+            return Money(amount: (self.amount / 3), currency: to)
+        }
+        else if (self.currency == "EUR" && to == "CAN") {
+            return Money(amount: (5 * (self.amount) / 6), currency: to)
+        }
+        else if (self.currency == "CAN" && to == "USD") {
+            return Money(amount: (4 * (self.amount) / 5), currency: to)
+        }
+        else if (self.currency == "CAN" && to == "GBP") {
+            return Money(amount: (2 * (self.amount) / 5), currency: to)
+        }
+        else if (self.currency == "CAN" && to == "EUR") {
+            return Money(amount: (6 * (self.amount) / 5), currency: to)
+        }
+        else  {
+            return Money(amount: (self.amount), currency: to)
+        }
+    }
   
-  public func add(to: Money) -> Money {
-  }
-  public func subtract(from: Money) -> Money {
-  }
+    public func add(to: Money) -> Money {
+        if (self.currency != to.currency) {
+            let converted = self.convert(to.currency)
+            return Money(amount: (converted.amount + to.amount), currency: to.currency)
+        } else {
+            return Money(amount: (self.amount + to.amount), currency: to.currency)
+        }
+    }
+    public func subtract(from: Money) -> Money {
+        if (self.currency != from.currency) {
+            let converted = self.convert(self.currency)
+            return Money(amount: (from.amount - converted.amount), currency: from.currency)
+        } else {
+            return Money(amount: (from.amount - self.amount), currency: from.currency)
+        }
+    }
 }
 
 ////////////////////////////////////
 // Job
 //
-public class Job {
-  public enum JobType {
-    case Hourly(Double)
-    case Salary(Int)
-  }
-  
-  public init(title : String, type : JobType) {
-  }
-  
-  public func calculateIncome(hours: Int) -> Int {
-  }
-  
-  public func raise(amt : Double) {
-  }
-}
-
-////////////////////////////////////
-// Person
+//public class Job {
+//    public enum JobType {
+//        case Hourly(Double)
+//        case Salary(Int)
+//    }
+//  
+//    public init(title : String, type : JobType) {
+//    }
+//  
+//    public func calculateIncome(hours: Int) -> Int {
+//        return 0
+//    }
+//  
+//    public func raise(amt : Double) {
+//    }
+//}
 //
-public class Person {
-  public var firstName : String = ""
-  public var lastName : String = ""
-  public var age : Int = 0
-
-  public var job : Job? {
-    get { }
-    set(value) {
-    }
-  }
-  
-  public var spouse : Person? {
-    get { }
-    set(value) {
-    }
-  }
-  
-  public init(firstName : String, lastName: String, age : Int) {
-    self.firstName = firstName
-    self.lastName = lastName
-    self.age = age
-  }
-  
-  public func toString() -> String {
-  }
-}
-
-////////////////////////////////////
-// Family
+//////////////////////////////////////
+//// Person
+////
+//public class Person {
+//    public var firstName : String = ""
+//    public var lastName : String = ""
+//    public var age : Int = 0
 //
-public class Family {
-  private var members : [Person] = []
-  
-  public init(spouse1: Person, spouse2: Person) {
-  }
-  
-  public func haveChild(child: Person) -> Bool {
-  }
-  
-  public func householdIncome() -> Int {
-  }
-}
+//    public var job : Job? {
+//        get { }
+//        set(value) {
+//        }
+//    }
+//  
+//    public var spouse : Person? {
+//        get { }
+//        set(value) {
+//        }
+//    }
+//  
+//    public init(firstName : String, lastName: String, age : Int) {
+//        self.firstName = firstName
+//        self.lastName = lastName
+//        self.age = age
+//    }
+//  
+//    public func toString() -> String {
+//    }
+//}
+//
+//////////////////////////////////////
+//// Family
+////
+//public class Family {
+//    private var members : [Person] = []
+//  
+//    public init(spouse1: Person, spouse2: Person) {
+//    }
+//  
+//    public func haveChild(child: Person) -> Bool {
+//    }
+//  
+//    public func householdIncome() -> Int {
+//    }
+//}
 
+let tenUSD = Money(amount: 10, currency: "USD")
+print("Currency: \(tenUSD.currency)" + " Amount: \(tenUSD.amount)")
 
+print("\nCONVERSION")
+let gbp = tenUSD.convert("GBP")
+print("Currency: \(gbp.currency)" + " Amount: \(gbp.amount)")
 
-
-
+print("\nADDITION")
+let fiveGBP = Money(amount: 5, currency: "GBP")
+let total = tenUSD.subtract(fiveGBP)
+print("Currency: \(total.currency)" + " Amount: \(total.amount)")
