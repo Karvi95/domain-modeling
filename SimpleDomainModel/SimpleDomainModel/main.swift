b3aad5448f4dddb2d8932effad4b2fe032ca2f80
karvi90 <karvi90@uw.edu>
2016-04-08 19:00:42 -0700

    Progessed but Questions remain

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -121,7 +121,7 @@ public class Job {
     public func raise(amt : Double) {
         switch self.type {
         case .Salary(let aSalary):
-            Double(aSalary) + amt
+            Double(aSalary) + amt         // Where is the initial ammount stored?
         case .Hourly(let rate):
             rate + amt
         }
@@ -131,48 +131,80 @@ public class Job {
 ////////////////////////////////////
 // Person
 //
-//public class Person {
-//    public var firstName : String = ""
-//    public var lastName : String = ""
-//    public var age : Int = 0
-//
-//    public var job : Job? {
-//        get { }
-//        set(value) {
-//        }
-//    }
-//  
-//    public var spouse : Person? {
-//        get { }
-//        set(value) {
-//        }
-//    }
-//  
-//    public init(firstName : String, lastName: String, age : Int) {
-//        self.firstName = firstName
-//        self.lastName = lastName
-//        self.age = age
-//    }
-//  
-//    public func toString() -> String {
-//    }
-//}
-//
+public class Person {
+    public var firstName : String = ""
+    public var lastName : String = ""
+    public var age : Int = 0
+
+    public var job : Job? {
+        get {
+            if (self.age >= 16) {
+                
+            } else {
+                return nil
+            }
+        }
+        set(value) {
+        }
+    }
+  
+    public var spouse : Person? {
+        get {
+            if (self.age >= 18) {
+                
+            } else {
+                return nil
+            }
+            
+        }
+        set(value) {
+        }
+    }
+  
+    public init(firstName : String, lastName: String, age : Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
+
+    public func toString() -> String {
+        return "[Person: firstName\(self.firstName) lastName:\(self.lastName) age:\(self.age) job:\(self.job?.title) spouse:\(self.spouse)]"
+    }
+}
+
 //////////////////////////////////////
 //// Family
-////
-//public class Family {
-//    private var members : [Person] = []
-//  
-//    public init(spouse1: Person, spouse2: Person) {
-//    }
-//  
-//    public func haveChild(child: Person) -> Bool {
-//    }
-//  
-//    public func householdIncome() -> Int {
-//    }
-//}
+//
+public class Family {
+    private var members : [Person] = []
+  
+    public init(spouse1: Person, spouse2: Person) {
+        if (spouse1.spouse == nil && spouse2.spouse == nil) {
+            
+        }
+    }
+  
+    public func haveChild(child: Person) -> Bool {  // Wait, if I'm supposed to add a new Person to the collection of age 0, why am I passed in a parameter?
+        var possible = false
+        for i in 0..<members.count {
+            if (members[i].age > 21) {
+                possible = true
+            }
+        }
+        members[members.count] = child
+        return possible
+    }
+  
+    public func householdIncome() -> Int {
+        var sum = 0
+        for i in 0..<members.count {
+            if (members[i].job != nil) {
+                sum += 1 //Figure out how to get at their Income
+            }
+        }
+        return sum
+    }
+}
 
 let job = Job(title: "Guest Lecturer", type: Job.JobType.Salary(1000))
 //print("Currency: \(job.title)" + " Amount: \(job.JobType)")
